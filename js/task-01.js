3ae3884810abe2e5f1659f468d0e44b87b6cfd3e
BohdanHromchak <gromchakbogdan@gmail.com>
2022-12-11 13:07:53 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const categoriesEl = document.querySelector("#categories");
+console.log("Number of categories:", categoriesEl.children.length);
+
+function getCategoryInfo() {
+  const titleArray = document.querySelectorAll("h2");
+  const categoryArray = document.querySelectorAll("#categories li ul");
+
+  for (let i = 0; i <= titleArray.length; i += 1) {
+    console.log("Category:", titleArray[i].textContent);
+    console.log("Elements:", categoryArray[i].children.length);
+  }
+}
+getCategoryInfo();
