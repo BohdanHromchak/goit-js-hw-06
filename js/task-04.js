12abe2c62aa91f8de35f4c0525a1a2b6dd5bd5cc
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-12 14:16:25 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,11 @@
+let counterValue = document.querySelector("#value");
+console.log(counterValue);
+// counterValue = 0;
+// const testmin = document.querySelector(".testmin");
+// const testmax = document.querySelector(".testmax");
+// testmin.addEventListener("click", onClickPlus);
+// // testmax.addEventListener("click", onClickMinus);
+
+// function onClickPlus(evt) {
+//   counterValue.textContent = "wtf";
+// }
