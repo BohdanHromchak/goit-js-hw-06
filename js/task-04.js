0d324c53abdf0b6c69b9b9cc1c875e324207a567
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-12 15:19:26 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -5,6 +5,10 @@ const buttonIncr = document.querySelector('[data-action="increment"]');
 
 let counterValue = 0;
 buttonDecr.addEventListener("click", () => {
+  counterValue -= 1;
+  counter.textContent = counterValue;
+});
+buttonIncr.addEventListener("click", () => {
   counterValue += 1;
   counter.textContent = counterValue;
 });
