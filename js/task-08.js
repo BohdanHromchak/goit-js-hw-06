7c2a49c88bf329c6dba4cb6a2143e340951c5963
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-13 12:50:14 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,15 @@
 const form = document.querySelector(".login-form");
-// const email = document.querySelector("[name='email']");
-// const password = document.querySelector("[name='password']");
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const { email, password } = evt.currentTarget.elements;
-  console.log(email.value);
+
   if (!email.value || !password.value) {
     return alert("Bсі поля повинні бути заповнені!");
   }
+  const enteredData = {};
+  enteredData.email = email.value;
+  enteredData.password = password.value;
+  console.log(enteredData);
+  form.reset();
 });
