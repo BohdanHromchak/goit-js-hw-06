4cca87e7849d873b20e47d5edbbaf5d500eb07fd
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-13 11:51:50 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const form = document.querySelector(".login-form");
+// const email = document.querySelector("[name='email']");
+// const password = document.querySelector("[name='password']");
 
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  console.log(email.value);
+  //   if () {
+  //     return alert("wtf");
+  //   }
+});
