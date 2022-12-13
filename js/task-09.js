e534f47a06ca601f43f424d40098258ab7eb178b
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-13 13:10:57 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+
+const changeBtn = document.querySelector(".change-color");
+console.log(changeBtn);
+changeBtn.addEventListener("click", (evt) => {
+  body.style.backgroundColor = getRandomHexColor();
+});
