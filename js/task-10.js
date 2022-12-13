f067531287686cf0d82391c54d8260922edeaea5
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-13 14:17:11 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,12 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// створення і очищення колекції елементів
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const input = document.querySelector("input");
 
 let enteredData = [];
-createBtn.addEventListener("click", (evt) => {
+createBtn.addEventListener("click", () => {
   enteredData.push(input.value);
   console.log(enteredData);
 });
@@ -14,3 +16,17 @@ destroyBtn.addEventListener("click", () => {
   enteredData = [];
   console.log(enteredData);
 });
+
+// Функція createBoxes(amount)
+const box = document.querySelector("#boxes");
+
+let markup = "";
+function createBoxes(amount) {
+  for (let i = 0; i <= amount; i += 1) {
+    markup += `<div></div>`;
+  }
+}
+createBoxes(5);
+box.insertAdjacentHTML("beforeend", markup);
+
+// Функція  destroyBoxes()
