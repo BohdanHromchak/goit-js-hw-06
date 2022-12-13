8878064bd6ff49033d0488f2c13e6226e770ecc0
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-13 14:01:36 +0200

    updated

--- js/task-09.js
+++ js/task-09.js
@@ -2,6 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const body = document.querySelector("body");
+const colorName = document.querySelector(".color");
 
 const changeBtn = document.querySelector(".change-color");
 console.log(changeBtn);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+
+let enteredData = [];
+createBtn.addEventListener("click", (evt) => {
+  enteredData.push(input.value);
+  console.log(enteredData);
+});
+destroyBtn.addEventListener("click", () => {
+  enteredData = [];
+  console.log(enteredData);
+});
