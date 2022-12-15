6a3ca1b2842cbd4e33015fc136b3197ddca70a39
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2022-12-15 15:12:22 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,10 @@
 const input = document.querySelector("#validation-input");
 input.addEventListener("blur", (evt) => {
   if (evt.currentTarget.value.length === 6) {
-    return input.classList.add("valid");
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
   }
-  input.classList.add("invalid");
 });
