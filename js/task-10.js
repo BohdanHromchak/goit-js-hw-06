55a3e8bba2d563ed8ab9c30f8210cc68cca7c9dc
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2023-01-27 15:59:13 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,19 +2,29 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const input = document.querySelector("input");
+const boxesList = document.querySelector("#boxes");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 
 createBtn.addEventListener("click", onCreate);
 destroyBtn.addEventListener("click", onDestroy);
 
-function onCreate() {}
-function onDestroy() {}
+function onCreate() {
+  createBoxes(input.value);
+}
+function onDestroy() {
+  boxesList.innerHTML = "";
+  input.value = "";
+}
 
 function createBoxes(amount) {
-  let boxSize = 30;
+  boxesList.innerHTML = "";
   let markup = "";
+  let boxWidth = 20;
+  let boxHeight = 20;
   for (let i = 1; i <= amount; i += 1) {
-    markup += `<div style="background-color: aquamarine; width: 30px; height: 30px"></div>`;
+    markup += `<div style="border: 2px solid; background-color: ${getRandomHexColor()}; width: ${(boxWidth += 10)}px; height: ${(boxHeight += 10)}px"></div>`;
   }
+  boxesList.insertAdjacentHTML("beforeend", markup);
 }
